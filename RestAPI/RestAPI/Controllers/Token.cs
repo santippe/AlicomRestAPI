91b3a95b72f82ee0e0b91f0f5d9c86f52c4091cd
Marco Mascioli <5377295+santippe@users.noreply.github.com>
2020-08-14 02:20:49 +0200

    Added JwtAuth

---
+++ RestAPI/RestAPI/Controllers/Token.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class Token : ControllerBase
+    {
+        [AllowAnonymous]
+        public string Get()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            string _privateKey = "12345678901234567890";
+            var key = Encoding.ASCII.GetBytes(_privateKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                   //new Claim(ClaimTypes.Name, "")
+                }),
+                Issuer = "https://localhost:5002",
+                Audience = "https://localhost:5001",
+                Expires = DateTime.UtcNow.AddDays(7),
+                IssuedAt = DateTime.UtcNow,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            string tokenValue = tokenHandler.WriteToken(token);
+            return tokenValue;
+        }
+
+
+        [Route("test")]        
+        public IActionResult TestToken()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ RestAPI/RestAPI/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace RestAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(opts =>
+                {
+                    string _privateKey = "12345678901234567890";
+                    var key = Encoding.ASCII.GetBytes(_privateKey);
+                    opts.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateLifetime = false,
+                        ValidateAudience = true,
+                        //ValidateIssuerSigningKey = false,
+                        ValidIssuer = "https://localhost:5002",
+                        ValidAudience = "https://localhost:5001",
+                        //EVER and FOREVER
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+            services.AddAuthorization();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
